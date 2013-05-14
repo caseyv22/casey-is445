4f9aa68989b74b4d9ae6a4779af39cc34e6c4eb5
Casey Villanueva <caseyvillanueva@gmail.com>
2013-05-13 21:41:42 -0700

    final project update

---
+++ Demos/ClientSide/Clientside.js
@@ -0,0 +1,12 @@
+﻿var userinput = prompt('Please enter a nuber between 2 and 10', '');
+if (userinput <= 10 && userinput >= 2) {
+    document.write('<h2>Your input number is ' + userinput + '</h2> <br />');
+    var millioncount = userinput;
+    for (var num = 0; millioncount > .000001; num++) {
+        millioncount /= 2;
+    }
+    document.write("<h2>The number of times to divide a number " + userinput + " by 2 to get a value less than one millionth is: " + num + "</h2>");
+}
+else {
+    document.write("<span style=\"color:green;\"><h3>Your input number is " + userinput + ". The valid input number is between 2 and 10. Please reload this page and try again.</h2><\/span>");
+}
---
+++ Demos/ClientSide/Nestedloop.js
@@ -0,0 +1,17 @@
+﻿var userinput = prompt('Please enter a nuber between 2 and 10', '');
+if (userinput <= 10 && userinput >= 2) {
+    document.write('<h2>Your input number is ' + userinput + '</h2> <br />');
+
+    var startnum = userinput;
+    document.write('<p>');
+    for (var startnum = userinput; startnum !== 0; startnum--) {
+        for (var startcount = startnum; startcount !== 0; startcount--) {
+            document.write("*");
+        }
+        document.write('<br />');
+    }
+    document.write('</p>');
+}
+else {
+    document.write("<h3>Your input number is ' + userinput + '. The valid input number is between 2 and 10. Please reload this page and try again.</h2>");
+}
